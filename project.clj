71e96f5bf7f5df5b34f43938cdd41dcdfc348d06
app <app@localhost.localdomain>
2014-02-20 16:38:30 +0000

    * Initial commit

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject dataproc "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
+                                 :username "peeta.shaw@gmail.com"
+                                 :password "39eb876d-aaf2-4343-8fa2-e02e919daaa7"}}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.datomic/datomic-pro "0.9.4556"]])
