ce39fc1f7d3b728cd4842c63234b5eb8e97c7113
Pete Shaw <peeta.shaw@gmail.com>
2014-03-14 14:47:58 +0000

    * Using fixed thread pool executor in DBScanner due to performance issues on single thread

--- src/dataproc/services/dbscanner.clj
+++ src/dataproc/services/dbscanner.clj
@@ -5,24 +5,25 @@
             [dataproc.db.datomic :as dbd]
             [immutant.messaging :as msg]
             [taoensso.timbre :as log]
-            [immutant.cache :as cache]))
+            [immutant.cache :as cache])
+  (:import  [java.util.concurrent Executors]))
 
-(def ^:private done (atom false))
+(def ^:private tpool (Executors/newFixedThreadPool 4))
 
 (def ^:private dcache (cache/lookup-or-create "dataproc" :tx false :persist "/app/dataproc/cache"))
+
+(defn workFn
+  []
+  (let [db (dbd/get-db)]
+          (msg/with-connection {}
+            (doseq [result (map :e (d/seek-datoms db :aevt :artist/name (get dcache :last-ref)))]
+                (log/debug (str "Publishing to work queue: " result))
+                (msg/publish "/queue/dataproc/work/" result)
+                (cache/put dcache :last-ref result)))))
 
 (defrecord DBScanner []
   daemon/Daemon
   (start [_]
-    (reset! done false)
-    (log/info "Initialising cache")
-    (loop [i 0]
-        (let [db (dbd/get-db)]
-          (msg/with-connection {}
-            (doseq [result (map :e (take 20000 (d/seek-datoms db :aevt :artist/name (get dcache :last-ref))))]
-                (log/debug (str "Publishing to work queue: " result))
-                (msg/publish "/queue/dataproc/work/" result)
-                (cache/put dcache :last-ref result))))
-        (Thread/sleep 120000)
-        (recur (inc i))))
-  (stop [_] (reset! done true)))
+    (.submit tpool workFn))
+  (stop [_]
+    (.shutdown tpool)))
