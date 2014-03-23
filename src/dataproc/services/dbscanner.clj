595d50b5e08c40809bc157e7642a9f735cdc135e
Pete Shaw <peeta.shaw@gmail.com>
2014-03-23 12:34:45 +0000

    * Fixed bug where multiple scanners were given the save IDs to scan

--- src/dataproc/services/dbscanner.clj
+++ src/dataproc/services/dbscanner.clj
@@ -42,34 +42,55 @@
      :last-ref start-ref}))
 
 (defn- active-scanners
-  "Doesn't quite work right yet"
+  "Returns the list of active scanners"
   []
   (loop [scanners (get dcache :scanners)
          result {}]
     (if (empty? scanners)
       result
-      (recur (rest scanners) (conj result (get dcache (keyword (first scanners))))))))
+      (recur (rest scanners) (assoc result (keyword (first scanners)) (get dcache (keyword (first scanners))))))))
 
-(defn- spawn-scanner
+(defn- create-scanner-with-params
   []
   (let [params (generate-work-params (get dcache :next-ref))
         uuid (gen-uuid)]
     (dosync
       (cache/put dcache (keyword uuid) params)
       (cache/swap! dcache :scanners conj uuid)
-      (workFn (conj params {:id uuid})))))
-    
+      #(workFn (assoc params :id uuid)))))
+
+(defn- spawn-scanners
+  ([tpool]
+    (let [scanner (create-scanner-with-params)]
+    (.submit tpool scanner)))
+  ([tpool num]
+    (dotimes [n num]
+        (.submit tpool (create-scanner-with-params)))))
+
+(defn- resume-scanners
+  [tpool scanner-params]
+  (for [key scanner-params
+        :let [params (get dcache (keyword key))]]
+    (.submit tpool #(workFn (assoc params :id key)))))
 
 (defrecord DBScanner []
   daemon/Daemon
   (start [_]
     (cache/put-if-absent dcache :scanners #{})
     (log/info "Starting DBScanner")
-    (.submit tpool spawn-scanner)
-    (.submit tpool spawn-scanner)
+    (let [active-scanners (active-scanners)
+          num-active-scanners (count active-scanners)]
+      (if (>= num-active-scanners 4)
+        (do
+          (log/info "Resuming" num-active-scanners "active scanners")
+          (resume-scanners tpool (:scanners dcache)))
+        (do
+          (log/info "Resuming" num-active-scanners "active scanners and spawning" (- 4 num-active-scanners) "additional scanners")
+          (resume-scanners tpool (:scanners dcache))
+          (spawn-scanners tpool (- 4 num-active-scanners)))))
     (loop []
-      (println (.countMessages (hornetq/destination-controller "/queue/dataproc/work/") nil))
-      (Thread/sleep 2000)
+      (log/report "Stats:" (.countMessages (hornetq/destination-controller "/queue/dataproc/work/") nil))
+      (Thread/sleep 10000)
       (recur)))
   (stop [_]
     (.shutdown tpool)))
--- test/site_specific/t_core.clj
+++ test/site_specific/t_core.clj
@@ -5,4 +5,5 @@
 (fact "`is-collab?` returns true when an artist collaborates on a song"
   (is-collab? "My great song") => false
   (is-collab? "My great song feat. Dr Dre") => true
-  (is-collab? "My great song featuring The Killers") => true)
+  (is-collab? "My great song featuring The Killers") => true
+  (is-collab? "My great song FEAT The Killers") => true)
